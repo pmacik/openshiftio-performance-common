f93823e934dc8c3141f095faa25a2547154e6079
Pavel Macík <pavel.macik@gmail.com>
2018-05-11 15:57:07 +0200

    Update the paths to be more precise.

--- __start-locust-master.sh
+++ __start-locust-master.sh
@@ -8,7 +8,7 @@ if [ -f $LOCUST_FILE ]; then
 	scp $LOCUST_FILE $SSH_USER@$MASTER_HOST:$SSH_WORKDIR/$JOB_BASE_NAME-$BUILD_NUMBER-locustfile.py;
 	ssh $SSH_USER@$MASTER_HOST "chmod u+w $SSH_WORKDIR/users.env"
 	scp $ENV_FILE-master $SSH_USER@$MASTER_HOST:$SSH_WORKDIR/users.env
-	CMD="rm -rf $SSH_WORKDIR/$LOG_DIR; mkdir -p $SSH_WORKDIR/$LOG_DIR/csv; nohup locust -f $JOB_BASE_NAME-$BUILD_NUMBER-locustfile.py --master --no-web --print-stats --no-reset-stats --expect-slaves=$SLAVES --clients=$USERS --hatch-rate=$USER_HATCH_RATE --csv=$LOG_DIR/csv/$JOB_BASE_NAME-$BUILD_NUMBER-report 2>$LOG_DIR/$JOB_BASE_NAME-$BUILD_NUMBER-locust-master.log > $LOG_DIR/$JOB_BASE_NAME-$BUILD_NUMBER-locust-master.log &";
+	CMD="rm -rf $SSH_WORKDIR/$LOG_DIR; mkdir -p $SSH_WORKDIR/$LOG_DIR/csv; nohup locust -f $JOB_BASE_NAME-$BUILD_NUMBER-locustfile.py --master --no-web --print-stats --no-reset-stats --expect-slaves=$SLAVES --clients=$USERS --hatch-rate=$USER_HATCH_RATE --csv=$SSH_WORKDIR/$LOG_DIR/csv/$JOB_BASE_NAME-$BUILD_NUMBER-report 2>$SSH_WORKDIR/$LOG_DIR/$JOB_BASE_NAME-$BUILD_NUMBER-locust-master.log > $SSH_WORKDIR/$LOG_DIR/$JOB_BASE_NAME-$BUILD_NUMBER-locust-master.log &";
 	echo "Starting a master at $MASTER_HOST: $CMD";
 	echo $CMD >> $$-master;
 	ssh $SSH_USER@$MASTER_HOST "source $SSH_WORKDIR/users.env; bash -s" < $$-master;
--- _locust-log-to-csv.sh
+++ _locust-log-to-csv.sh
@@ -4,13 +4,13 @@
 
 METRIC=$1;
 INPUT=$2;
-TMPFILE=${3-$JOB_BASE_NAME-$BUILD_NUMBER-`echo $METRIC | sed -e 's,[ /?:]\+,_,g'`.csv};
 OUTPUT=${3-$LOG_DIR/csv/$JOB_BASE_NAME-$BUILD_NUMBER-`echo $METRIC | sed -e 's,[ /?:]\+,_,g'`.csv};
+TMPFILE=$OUTPUT.tmp
 
 echo $OUTPUT
 echo -n "" > $OUTPUT
 
-header="TimeInSeconds;"`cat $INPUT | grep "Name" | head -n 1 | sed -e 's,\s\s*|\?\s\+,;,g' | cut -d ";" -f 2-8`;
+header="TimeInSeconds;"$(cat $INPUT | grep "Name" | head -n 1 | sed -e 's,\s\s*|\?\s\+,;,g' | cut -d ";" -f 2-8);
 
 cat $INPUT | while read line; do
 	if [[ $line == "Percentage of the requests completed within given times"* ]]; then
@@ -23,7 +23,7 @@ done
 pseudo_now=$DURATION
 tac $$-$TMPFILE | while read line; do
 	echo "$pseudo_now;$line" >> $$-$TMPFILE.new
-	pseudo_now=`expr $pseudo_now - 2`
+	pseudo_now=$(expr $pseudo_now - 2)
 done
 
 echo $header>$OUTPUT
