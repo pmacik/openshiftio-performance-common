d6129669301def6382885079b9a6b85261b977e6
Pavel Macík <pavel.macik@gmail.com>
2018-05-11 13:15:07 +0200

    Fix separating logs

--- _csv-failures-to-png.sh
+++ _csv-failures-to-png.sh
@@ -2,17 +2,20 @@
 
 #source _setenv.sh
 
-NAME=`echo $1 | sed -e 's/csv/png/' | sed -e 's,\.csv,,g'`
+mkdir -p $LOG_DIR/png
+
+INPUT=$1
+NAME=`echo $INPUT | xargs -I{} basename {} | sed -e 's,\.csv,,g'`
 
 gnuplot << eor
 set terminal png size $REPORT_CHART_WIDTH, $REPORT_CHART_HEIGHT noenhanced
 set title "$NAME Failures"
-set output "$NAME-failures.png"
+set output "$LOG_DIR/png/$NAME-failures.png"
 set style data line
 set yrange [0:*]
 set datafile separator ";"
 set xlabel "Time [s]"
 set ylabel "# of Failures"
 set grid
-plot "$1" u 1:3 t "# of failures"
+plot "$INPUT" u 1:3 t "# of failures"
 eor
--- _csv-throughput-to-png.sh
+++ _csv-throughput-to-png.sh
@@ -2,17 +2,19 @@
 
 #source _setenv.sh
 
-NAME=`echo $1 | sed -e 's/csv/png/' | sed -e 's,\.csv,,g'`
+mkdir -p $LOG_DIR/png
+INPUT=$1
+NAME=`echo $INPUT | xargs -I{} basename {} | sed -e 's,\.csv,,g'`
 
 gnuplot << eor
 set terminal png size $REPORT_CHART_WIDTH, $REPORT_CHART_HEIGHT noenhanced
 set title "$NAME Throughput"
-set output "$NAME-throughput.png"
+set output "$LOG_DIR/png/$NAME-throughput.png"
 set style data line
 set yrange [0:*]
 set datafile separator ";"
 set xlabel "Time [s]"
 set ylabel "req/s"
 set grid
-plot "$1" u 1:8 t "Average"
+plot "$INPUT" u 1:8 t "Average"
 eor
