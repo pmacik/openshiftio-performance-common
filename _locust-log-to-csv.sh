dfd2a8b47c3c86e15fc64f60637ef8dcce206397
Pavel Macík <pavel.macik@gmail.com>
2018-05-14 12:06:14 +0200

    Fix outputdir and tmpfile handling in _locust-log-to-csv.sh

--- _locust-log-to-csv.sh
+++ _locust-log-to-csv.sh
@@ -4,11 +4,12 @@
 
 METRIC=$1;
 INPUT=$2;
-OUTPUT=${3-$LOG_DIR/csv/$JOB_BASE_NAME-$BUILD_NUMBER-`echo $METRIC | sed -e 's,[ /?:]\+,_,g'`.csv};
+OUTPUT_DIR=${3:-$LOG_DIR/csv}
+OUTPUT=${4:-$JOB_BASE_NAME-$BUILD_NUMBER-`echo $METRIC | sed -e 's,[ /?:]\+,_,g'`.csv};
 TMPFILE=$OUTPUT.tmp
 
-echo $OUTPUT
-echo -n "" > $OUTPUT
+echo $OUTPUT_DIR/$OUTPUT
+echo -n "" > $OUTPUT_DIR/$OUTPUT
 
 header="TimeInSeconds;"$(cat $INPUT | grep "Name" | head -n 1 | sed -e 's,\s\s*|\?\s\+,;,g' | cut -d ";" -f 2-8);
 
@@ -16,17 +17,17 @@ cat $INPUT | while read line; do
 	if [[ $line == "Percentage of the requests completed within given times"* ]]; then
 		break;
 	else
-		echo $line | grep -F "$METRIC" | sed -e "s,[\( \)\(|\)\]\+,;,g" | cut -d ";" -f 3-4,6-11 >> $$-$TMPFILE
+		echo $line | grep -F "$METRIC" | sed -e "s,[\( \)\(|\)\]\+,;,g" | cut -d ";" -f 3-4,6-11 >> $OUTPUT_DIR/$$-$TMPFILE
 	fi
 done
 
 pseudo_now=$DURATION
-tac $$-$TMPFILE | while read line; do
-	echo "$pseudo_now;$line" >> $$-$TMPFILE.new
+tac $OUTPUT_DIR/$$-$TMPFILE | while read line; do
+	echo "$pseudo_now;$line" >> $OUTPUT_DIR/$$-$TMPFILE.new
 	pseudo_now=$(expr $pseudo_now - 2)
 done
 
-echo $header>$OUTPUT
-tac $$-$TMPFILE.new >> $OUTPUT
+echo $header>$OUTPUT_DIR/$OUTPUT
+tac $OUTPUT_DIR/$$-$TMPFILE.new >> $OUTPUT_DIR/$OUTPUT
 
-rm -rf $$-*
+rm -rf $OUTPUT_DIR/$$-*
